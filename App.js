b0829449b2a1c7c256410c1be79827c927cb4e28
shivasathya <98640101+shivasathya@users.noreply.github.com>
2022-03-04 19:59:41 +0530

    uploaded

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import ButtonDisplay from "./Button/ButtonDisplay"
+
+ export const App =()=>{
+    return (
+        <div>
+             <h1 style={{textAllign:"center"}}>Fruits Center</h1>
+             <ButtonDisplay/ >
+
+        </div>
+    )
+}
+
+
+export default App
